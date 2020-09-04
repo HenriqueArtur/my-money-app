8f1fc562e341ade4dcc86c3939c0d584727783b4
Henrique Artur <contato@henriqueartur.com>
2020-09-04 20:31:11 -0300

    add redux

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import promise from 'redux-promise'
+
+import reducers from './main/reducers'
 import App from './main/app'
 
+const store = applyMiddleware(promise)(createStore)(reducers)
 ReactDom.render(
-    <App />,
-    document.getElementById('app')
+    <Provider store={store}>
+        <App />
+    </Provider>
+    ,document.getElementById('app')
 )
