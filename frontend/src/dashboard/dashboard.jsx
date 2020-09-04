722596f49cd213a9180fe55dc01b860b365a8d6d
Henrique Artur <contato@henriqueartur.com>
2020-09-04 14:54:45 -0300

    add grid

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 import Content from '../common/template/content'
 import ContentHeader from '../common/template/contentHeader'
-import content from '../common/template/content'
 
 class Dashboard extends Component {
     render() {
