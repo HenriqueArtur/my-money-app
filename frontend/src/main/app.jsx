6b15e4de7f6759e4460c18f12430ffee10d429eb
Henrique Artur <contato@henriqueartur.com>
2020-09-04 11:40:44 -0300

    create app react

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <div className="weapper">
+        <h1>App</h1>
+    </div>
+)
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,8 +1,9 @@
+  
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entre: './src/index.jsx',
+    entry: './src/index.jsx',
     output: {
         path: __dirname + '/public',
         filename: './app.js'
@@ -12,17 +13,17 @@ module.exports = {
         contentBase: './public',
     },
     resolve: {
-        extensions: ['', '.js', './jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugin: [
-        new webpack.ProgressPlugin({
+    plugins: [ 
+        new webpack.ProvidePlugin({
             $: 'jquery',
-            jquery: 'jquery',
+            jQuery: 'jquery',
             'window.jQuery': 'jquery'
         }),
         new ExtractTextPlugin('app.css')
