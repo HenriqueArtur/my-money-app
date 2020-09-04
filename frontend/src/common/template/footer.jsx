648d2f3aeed290c54c1309d4d8922dbf7cb9d46c
Henrique Artur <contato@henriqueartur.com>
2020-09-04 14:03:14 -0300

    add footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2020 - {(new Date().getFullYear())} | Feito com 💖 por <a href="http://www.henriqueartur.com" target="_blank">Henrique Artur</a>.
+        </strong>
+        <span className="social-icons">
+            <a href="https://github.com/HenriqueArtur" target="_blank">
+                <i className="fa fa-github-alt"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/henriqueartur/" target="_blank">
+                <i className="fa fa-linkedin"></i>
+            </a>
+            <a href="mailto:contato@henriqueartur.com">
+                <i className="fa fa-envelope"></i>
+            </a>
+        </span>
+    </footer>
+)
