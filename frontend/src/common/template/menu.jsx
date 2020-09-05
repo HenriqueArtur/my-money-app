63fe745365530acbb03ae4ca74309893da3b1c43
Henrique Artur <contato@henriqueartur.com>
2020-09-04 23:30:55 -0300

    create tabs components

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -5,7 +5,7 @@ import MenuTree from './menuTree'
 export default props => (
     <ul className="sidebar-menu">
         <MenuItem path='#' label='Dashboard' icon='dashboard' />
-        <MenuTree label='cadastro' icon='edit'>
+        <MenuTree label='Cadastro' icon='edit'>
             <MenuItem path='#billingCycles' label='Ciclo de Pagamentos' icon='usd' />
         </MenuTree>
     </ul>
